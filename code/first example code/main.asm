7f46022fe1f1668e1e08a9be9d7889878b778f21
chirs2972 <chirs2972@yahoo.com.tw>
2018-06-24 14:29:33 +0800

    vs project with main
    set my vs assembler with first code "main.asm"
    
    placing first code at code

---
+++ code/first example code/main.asm
@@ -0,0 +1,20 @@
+INCLUDE Irvine32.inc
+
+.data
+val1 dword 10000h
+val2 dword 40000h
+val3 dword 20000h
+finalVal dword ?
+
+.code
+main PROC
+
+	mov eax,val1  ;
+	add eax,val2  ;
+	sub eax,val3  ;
+	mov finalVal,eax  ;
+	call DumpRegs ;
+
+	exit
+main ENDP
+END main
